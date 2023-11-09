6f77cd18cdb1f5a7a66e2c6c4a737dc20bbe9d59
Michele Maffei <michele.muffin@gmail.com>
2023-11-09 12:00:21 +0100

    Fix Sano font import, add fallback

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google'
 import { Sono } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] })
-export const sono = Sono({ subsets: ['latin'], weight: ['400']})
+export const sono = Sono({ subsets: ['latin'], fallback: ['monospace'] })
