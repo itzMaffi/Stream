3801e451e943950ad976992e6b377eed53fffbc2
Michele Maffei <michele.muffin@gmail.com>
2023-11-09 11:12:48 +0100

    Reorganize directory structure, move globals.css to ui, add fonts.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { inter } from './ui/fonts'
+import './ui/globals.css'
 
 export const metadata: Metadata = {
   title: 'Stream',
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google'
+import { Sono } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const sono = Sono({ subsets: ['latin'], weight: ['400']})
