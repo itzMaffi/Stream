b4cfe5bcb5338fe6914ecc4c81b12e4a88e1d85e
Michele Maffei <michele.muffin@gmail.com>
2023-11-09 17:51:54 +0100

    Finish keyboard event intercept, add recording state and timeline

--- app/ui/thought-input.tsx
+++ app/ui/thought-input.tsx
@@ -1,13 +1,32 @@
 'use client'
 
 import { sono } from "./fonts"
-import { ChangeEvent, KeyboardEvent } from 'react';
+import { ChangeEvent, KeyboardEvent, use, useRef, useState } from 'react';
 
 const COLS = 30;
 const ROWS = 15;
 const MAX_LENGTH = COLS * ROWS;
 
+type Snapshot = {
+  ms: number
+  text: string
+}
+
 export default function ThoughtInput() {
+
+  const [recorder, setRecorder] = useState({recording: false, startTime: -1});
+  const timelineRef = useRef<Snapshot[]>([])
+
+  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+
+    if (!recorder.recording) {
+      setRecorder(prevRecorder => ({...prevRecorder, recording: true, startTime: Date.now()}));
+      timelineRef.current.push({ms: 0, text: event.target.value});
+    }
+
+    if (recorder.recording) timelineRef.current.push({ms: Date.now() - recorder.startTime, text: event.target.value});
+    console.log(timelineRef.current);
+  }
 
   function handleKeyDown(event: KeyboardEvent<HTMLTextAreaElement>) {
 
@@ -31,6 +50,9 @@ export default function ThoughtInput() {
   }
 
   return (
-    <textarea className={`rounded-md p-4 shadow-lg focus:outline-none resize-none ${sono.className}  text-lg break-all whitespace-break-spaces`} cols={COLS} rows={ROWS} maxLength={MAX_LENGTH} onKeyDown={handleKeyDown} wrap="hard" autoComplete="off" autoCorrect="off" autoFocus={true}></textarea>
+    <div className="flex flex-col">
+      <textarea className={`rounded-md p-4 shadow-lg focus:outline-none resize-none ${sono.className}  text-lg break-all whitespace-break-spaces`} cols={COLS} rows={ROWS} maxLength={MAX_LENGTH} onKeyDown={handleKeyDown} onChange={handleChange} wrap="hard" autoComplete="off" autoCorrect="off" autoFocus={true}></textarea>
+      <button className="my-8 ">Stop</button>
+    </div>
   )
 }
