807b441f22f336038498c52a93ce29d623fae22c
Michele Maffei <michele.muffin@gmail.com>
2023-11-09 17:49:43 +0100

    Change base font to quicksand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { inter } from './ui/fonts'
+import { quicksand } from './ui/fonts'
 import './ui/globals.css'
 
 export const metadata: Metadata = {
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`h-screen ${inter.className} `}>{children}</body>
+      <body className={`h-screen ${quicksand.className} `}>{children}</body>
     </html>
   )
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google'
+import { Quicksand } from 'next/font/google'
 import { Sono } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin'] })
+export const quicksand = Quicksand({ subsets: ['latin'] })
 export const sono = Sono({ subsets: ['latin'], fallback: ['monospace'] })
