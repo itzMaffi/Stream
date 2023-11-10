c9d2ab202bad10cced3482201bb7943eec6513ad
Michele Maffei <michele.muffin@gmail.com>
2023-11-10 19:41:35 +0100

    Add data fetching

--- app/history/[id]/visit/page.tsx
+++ app/history/[id]/visit/page.tsx
@@ -1,5 +1,22 @@
-export default function Visit() {
+import prisma from "@/app/lib/db";
+import ThoughtReplay from "@/app/ui/thought-replay";
+
+export default async function Visit({params}: {params: {id: string}}) {
+
+  const thought = await prisma.thought.findUnique({
+    where: {
+      id: params.id,
+    },
+    select: {
+      thoughtString: true,
+      thoughtTimeline: true,
+      createdAt: true,
+    }
+  })
+
   return (
-    <h1>Hello world</h1>
+    <div className="h-full flex items-center justify-center">
+      {thought && <ThoughtReplay thought={thought}></ThoughtReplay>}
+    </div>
   )
 }
