a25b5b5be9ea65d8f00b78c2d85cc792e1034cf7
Michele Maffei <michele.muffin@gmail.com>
2023-11-10 18:35:17 +0100

    Change folder structure, add on hover on link button

---
+++ app/history/[id]/visit/page.tsx
@@ -0,0 +1,5 @@
+export default function Visit() {
+  return (
+    <h1>Hello world</h1>
+  )
+}
--- app/ui/thought-present.tsx
+++ app/ui/thought-present.tsx
@@ -1,6 +1,8 @@
 import { sono } from "./fonts"
+import { FaBookOpen } from 'react-icons/fa6'
 import moment from "moment";
 import prisma from "../lib/db"
+import Link from "next/link";
 
 const COLS = 30;
 const ROWS = 15;
@@ -15,9 +17,21 @@ export default async function ThoughtPresent({id}: {id: string}) {
   })
 
   return (
-    <div className="m-4 snap-center shrink-0">
+    <div className="m-4 relative snap-center shrink-0 group">
       <p className="py-2 text-slate-400">{moment(thought?.createdAt).format('ddd MMM D YYYY hh:mm A')}</p>
-      <textarea className={`rounded-md p-4 shadow-lg focus:outline-none resize-none ${sono.className} text-lg break-all whitespace-break-spaces`} cols={COLS} rows={ROWS} maxLength={MAX_LENGTH} name="thought" defaultValue={thought?.thoughtString}disabled></textarea>
+      <textarea 
+        className={`rounded-md p-8 shadow-lg resize-none ${sono.className} text-lg break-all whitespace-break-spaces`} 
+        cols={COLS} 
+        rows={ROWS} 
+        maxLength={MAX_LENGTH} 
+        name="thought" 
+        defaultValue={thought?.thoughtString}
+        disabled
+      ></textarea>
+      <Link
+        href={`/history/${id}/visit`}
+        className="absolute bottom-2 right-2 invisible group-hover:visible p-2 bg-teal-400 hover:bg-teal-500 rounded-full text-white font-semibold"
+      ><FaBookOpen/></Link>
     </div>
   )
 }
