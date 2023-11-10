788b2ed2917039eecd2e98513230c44c460425af
Michele Maffei <michele.muffin@gmail.com>
2023-11-10 15:16:10 +0100

    Add thought presenter with routing

---
+++ app/history/page.tsx
@@ -0,0 +1,11 @@
+import ThoughtPresent from "../ui/thought-present";
+
+export default function History() {
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <ThoughtPresent id={"lol"}></ThoughtPresent>
+    </div>
+  )
+  
+}
---
+++ app/ui/thought-present.tsx
@@ -0,0 +1,5 @@
+export default async function ThoughtPresent({id}: {id: string}) {
+  return (
+    <h1>HELLO WORLD {id}</h1>
+  )
+}
