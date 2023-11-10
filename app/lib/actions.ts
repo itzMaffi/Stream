2e3de621c464c49bcbb3b263d44dd069fa3bd3b4
Michele Maffei <michele.muffin@gmail.com>
2023-11-10 13:39:14 +0100

    Update save thought server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,13 @@
 'use server'
 
 import { Snapshot } from './types/snapshot'
+import prisma from './db'
 
 export async function saveThought(timeline: Snapshot[], thought: string) {
-  
+  await prisma.thought.create({
+    data: {
+      thoughtString: thought,
+      thoughtTimeline: JSON.stringify(timeline)
+    }
+  });
 }
