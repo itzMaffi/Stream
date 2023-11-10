13c080aaa42079a9701f83ae5c55a975e469ca86
Michele Maffei <michele.muffin@gmail.com>
2023-11-10 16:37:04 +0100

    Add query for thought id and thought

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,10 +1,22 @@
+import prisma from "../lib/db";
 import ThoughtPresent from "../ui/thought-present";
 
-export default function History() {
+export default async function History() {
+  
+  const idObjs = await prisma.thought.findMany({
+    select: {
+      id: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
 
   return (
-    <div className="h-full flex items-center justify-center">
-      <ThoughtPresent id={"lol"}></ThoughtPresent>
+    <div className="h-full flex items-center">
+      {
+        idObjs && idObjs.map( idObj => <ThoughtPresent key={idObj.id} id={idObj.id}></ThoughtPresent>)
+      }
     </div>
   )
   
--- app/ui/thought-present.tsx
+++ app/ui/thought-present.tsx
@@ -1,5 +1,22 @@
+import { sono } from "./fonts"
+import prisma from "../lib/db"
+
+const COLS = 30;
+const ROWS = 15;
+const MAX_LENGTH = COLS * ROWS;
+
 export default async function ThoughtPresent({id}: {id: string}) {
+
+  const thought = await prisma.thought.findUnique({
+    where: {
+      id: id,
+    }
+  })
+
   return (
-    <h1>HELLO WORLD {id}</h1>
+    <div className="m-4">
+      <p className="py-2 text-slate-400">{thought?.createdAt.toString()}</p>
+      <textarea className={`rounded-md p-4 shadow-lg focus:outline-none resize-none ${sono.className} text-lg break-all whitespace-break-spaces`} cols={COLS} rows={ROWS} maxLength={MAX_LENGTH} name="thought" defaultValue={thought?.thoughtString}disabled></textarea>
+    </div>
   )
 }
