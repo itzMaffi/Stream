2e5561e7ac9b2036c8159964cfdca5060155904a
Michele Maffei <michele.muffin@gmail.com>
2023-11-13 13:20:59 +0100

    Delete duplicate files

--- app/(new)/page.tsx
+++
@@ -1,9 +0,0 @@
-import ThoughtInput from "../ui/thought-input"
-
-export default function New() {
-  return (
-    <div className="h-full flex items-center justify-center">
-      <ThoughtInput></ThoughtInput>
-    </div>
-  )
-}
