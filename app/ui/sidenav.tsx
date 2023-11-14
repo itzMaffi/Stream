5b7529033b940cdd2b05158c23d17561ccf34d11
Michele Maffei <michele.muffin@gmail.com>
2023-11-14 12:28:07 +0100

    Add custom gradient background to tailwind config, add gradient background to sidevan element

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -3,9 +3,9 @@ import NavLinks from './nav-links';
 export default function Sidenav() {
   return (
     <div className="w-48 h-full p-4 flex flex-col gap-2">
-      <div className="h-40 p-4 rounded-2xl bg-stream-200">STREAM</div>
+      <div className="h-40 p-4 rounded-2xl bg-dawn-pattern">STREAM</div>
       <NavLinks />
-      <div className={`grow rounded-2xl bg-stream-200`}></div>
+      <div className={`grow rounded-2xl bg-stream-50`}></div>
     </div>
   );
 }
