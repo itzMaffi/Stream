e4b6997b8f50f0049a621a7a3ef062c780c6f601
Michele Maffei <michele.muffin@gmail.com>
2023-11-14 12:24:43 +0100

    Add delete thought servera action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,24 @@
-'use server'
+'use server';
 
-import { Snapshot } from './types/snapshot'
-import prisma from './db'
+import { Snapshot } from './types/snapshot';
+import prisma from './db';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function saveThought(timeline: Snapshot[], thought: string) {
   await prisma.thought.create({
     data: {
       thoughtString: thought,
-      thoughtTimeline: JSON.stringify(timeline)
-    }
+      thoughtTimeline: JSON.stringify(timeline),
+    },
   });
+
+  revalidatePath('/dashboard/history');
+}
+
+export async function deleteThought(id: string) {
+  await prisma.thought.delete({ where: { id: id } });
+
+  revalidatePath('/dashboard/history');
+  redirect('/dashboard/history');
 }
