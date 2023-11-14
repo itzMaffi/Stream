b032093e4bf4255f65e18887c182c4336b0e727c
Michele Maffei <michele.muffin@gmail.com>
2023-11-14 14:47:39 +0100

    Add links container in preparation for responsive style

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -2,10 +2,16 @@ import NavLinks from './nav-links';
 
 export default function Sidenav() {
   return (
-    <div className="w-48 h-full p-4 flex flex-col gap-2">
-      <div className="h-40 p-4 rounded-2xl bg-dawn-pattern">STREAM</div>
-      <NavLinks />
-      <div className={`grow rounded-2xl bg-stream-50`}></div>
+    <div className="md:w-48 md:h-full p-4 flex flex-col gap-2">
+      <div className="md:h-40 p-4 rounded-2xl bg-dawn-pattern flex">
+        <h1 className="text-white self-end text-4xl leading font-semibold">
+          stream
+        </h1>
+      </div>
+      <div className="flex gap-2 addmd:flex-col flex-row">
+        <NavLinks />
+      </div>
+      <div className={`hidden md:block grow rounded-2xl bg-stream-50`}></div>
     </div>
   );
 }
