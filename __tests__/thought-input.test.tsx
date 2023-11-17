46d31f6aabf1900f5a78763d4cfd7532adc65a59
Mads Baadsmand <baadsmand@gmail.com>
2023-11-17 17:18:21 +0100

    tests: add thought input test: textarea exists and tab is 4 spaces

--- __tests__/thought-input.test.tsx
+++ __tests__/thought-input.test.tsx
@@ -1,14 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import ThoughtInput from "../app/ui/thought-input";
+import userEvent from '@testing-library/user-event';
 
-describe("Home", () => {
-  it("renders a heading", () => {
+describe("Thought Input", () => {
+  it("render the text area", async () => {
     render(<ThoughtInput />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
+    const textarea = screen.getByRole("textbox") as HTMLTextAreaElement;
 
-    expect(heading).toBeInTheDocument();
+    expect(textarea).toBeInTheDocument();
   });
+  
+  it("adds 4 spaces instead of a tab", async () => {
+    render(<ThoughtInput />);
+
+    const textarea = screen.getByRole("textbox") as HTMLTextAreaElement;
+
+    await userEvent.type(textarea,'Hello{tab}world');
+
+    expect(textarea.value).toBe("Hello    world");
+  });
+
+  
 });
