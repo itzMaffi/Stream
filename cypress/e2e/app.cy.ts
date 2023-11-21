9ffea2633e51dadbf3c55149dcba857cef649445
Guangzheng Li <guangzhengli@MacBook-Pro-2.local>
2023-11-21 12:00:27 +0100

    set the cypress env

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -8,17 +8,20 @@ describe("Navigation", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-  it("should navigate to the about page", () => {
-    // Start from the index page
 
+  it("should navigate to the main page", () => {
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click();
+    // cy.get('a[href*="about"]').click();
 
-    // The new url should include "/about"
-    cy.url().should("include", "/about");
+    // // The new url should include "/about"
+    // cy.url().should("include", "/about");
 
     // The new page should contain an h1 with "About page"
-    cy.get("h1").contains("About Page");
+    cy.get(".h-full > .hidden").contains("New");
+    cy.get('[href="/history"]')
+      .contains("History")
+      .should("exist")
+      .click();
   });
 });
 
